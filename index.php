d8e50d677ff45afd649560c5c3582de37fff2c27
VimukthiSathsara <63345225+VimukthiSathsara@users.noreply.github.com>
2020-12-12 05:05:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
            		   </div>
            		   <div class="div">
            		   		<h5>Username</h5>
-           		   		<input type="text" name="username" class="input">
+           		   		<input type="text" name="username" class="input" required>
            		   </div>
            		</div>
            		<div class="input-div pass">
@@ -28,7 +28,7 @@
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" name="password" class="input">
+           		    	<input type="password" name="password" class="input" required>
             	   </div>
             	</div>
             	<a href="#">Forgot Password?</a>
